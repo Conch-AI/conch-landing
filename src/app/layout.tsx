6b90776295396d149a7365be5931d7ec374db179
Sanjeev Shankar <sanjeev.19kr@gmail.com>
2026-02-12 22:02:21 +0530

    feat: add Open Graph and Twitter metadata for enhanced sharing, update application title and description, add new OG image, and remove deprecated favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,13 +3,33 @@ import "../app/styles/globals.css";
 import { ClientProviders } from "./providers";
 import { SessionProvider } from "@/context/SessionContext";
 
+
 export const metadata: Metadata = {
-  title: "Conch AI - Free AI Tools",
-  description: "Free AI tools to help you write, study, and learn. Simplify complex topics, humanize AI text, create flashcards, and more.",
+  title: "Conch AI - Your Undetectable AI Essay Writing and Research Assistant",
+  description: "Access every AI school tool in one to enhance your writing and research, without the busywork. Write, cite, and edit research papers, generate mind maps, notes, and flashcards from anything, or check for and bypass AI detection by humanizing your text.",
   icons: {
     icon: "/images/logos/logo_w_background.png",
     shortcut: "/images/logos/logo_w_background.png",
     apple: "/images/logos/logo_w_background.png",
+  },
+  openGraph: {
+    title: "Conch AI - Your Undetectable AI Essay Writing and Research Assistant",
+    description: "Access every AI school tool in one to enhance your writing and research, without the busywork. Write, cite, and edit research papers, generate mind maps, notes, and flashcards from anything, or check for and bypass AI detection by humanizing your text.",
+    images: [
+      {
+        url: "/images/og.jpeg",
+        width: 1200,
+        height: 630,
+        alt: "Conch AI Logo",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Conch AI - Your Undetectable AI Essay Writing and Research Assistant",
+    description: "Access every AI school tool in one to enhance your writing and research, without the busywork. Write, cite, and edit research papers, generate mind maps, notes, and flashcards from anything, or check for and bypass AI detection by humanizing your text.",
+    images: ["/images/logos/logo_w_background.png"],
   },
 };
 
