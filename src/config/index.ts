d9f14aabcc7c5573e3c7518a0bf1ebc897902918
Sanjeev Shankar <sanjeev.19kr@gmail.com>
2026-02-12 22:31:05 +0530

    fix: update API_BASE_URL to point to the correct production endpoint for improved API connectivity

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,4 +4,4 @@ export const Config = {
     | "prod",
 };
 
-export const API_BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://api.getconch.ai";
+export const API_BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://app.getconch.ai";
